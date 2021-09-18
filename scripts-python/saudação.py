92ed93875e6dba51034208a96f119fb92be2d564
Andre Mesquita <mesquitas@hotmail.com>
2021-09-17 21:03:46 -0300

    ex02

---
+++ scripts-python/saudação.py
@@ -0,0 +1,3 @@
+nome = input ("Qual é seu nome")
+print("Bem vindo",nome, "ao curso python")
+
---
+++ scripts-python/teste02.py
@@ -0,0 +1,7 @@
+d = input ("Dia ")
+m = input ("Mês ")
+a = input ("Ano ")
+print("Você nasceu no dia, ",d, "de " , m,  "de , " , a)
+
+print("Está correto? S/N")
+
