e3b328bd6d0486733a08f575bec97855bb5a7b86
Andre Mesquita <mesquitas@hotmail.com>
2021-09-17 23:45:34 -0300

    atualizacaopastas

--- scripts-python/nascimento.py
+++ scripts-python/nascimento.py
@@ -1,6 +1,6 @@
-d = input ("Dia ")
-m = input ("Mês ")
-a = input ("Ano ")
+d = int( input ("Dia "))
+m = int( input ("Mês "))
+a = int( input ("Ano "))
 print("Você nasceu no dia, ",d, "de " , m,  "de , " , a)
 
 print("Está correto? S/N")
